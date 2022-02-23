5b7a04eaf521510501f585d0725681ac78d1f274
David Orozco <orozco.davidj02@gmail.com>
2022-02-23 14:32:59 -0500

    feat: support to prebuilt images objects

---
+++ app/src/main/java/DuGraphics/ui/UIBufferedObject.java
@@ -0,0 +1,25 @@
+package DuGraphics.ui;
+
+import DuGraphics.states.State;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public abstract class UIBufferedObject extends UIObject {
+
+    private BufferedImage bufferedObject;
+
+    public UIBufferedObject(State parent, float x, float y, int width, int height) {
+        super(parent, x, y, width, height);
+
+        bufferedObject = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        create(bufferedObject.createGraphics());
+    }
+
+    protected abstract void create(Graphics2D graphics2D);
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(bufferedObject, (int) x, (int) y, width, height, null);
+    }
+}
