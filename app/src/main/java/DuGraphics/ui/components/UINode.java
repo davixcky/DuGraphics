e70668bf3537f87bf52e471500dd8c83663b9890
David Orozco <orozco.davidj02@gmail.com>
2022-02-24 16:55:01 -0500

    feat: add fade effect when node is created

--- app/src/main/java/DuGraphics/ui/components/UINode.java
+++ app/src/main/java/DuGraphics/ui/components/UINode.java
@@ -12,6 +12,10 @@ public class UINode extends UIBufferedObject {
     private final BSTNode nodeData;
     private float nextX, nextY;
 
+    private int currentStatus;
+
+    private Color mainColor;
+
     public UINode(State parent, float x, float y, int width, int height, BSTNode node) {
         super(parent, x, y, width, height);
         this.nodeData = node;
@@ -19,12 +23,14 @@ public class UINode extends UIBufferedObject {
         nextX = x;
         nextY = y;
         bounds.setBounds(0, 0, width + 30, height + 30);
+        currentStatus = 0;
+        mainColor = new Color(255, 0, 0, 0);
         init();
     }
 
     @Override
     protected void create(Graphics2D g2) {
-        g2.setColor(Color.red);
+        g2.setColor(mainColor);
         g2.fillOval(0, 0, width, height);
         g2.setColor(Color.white);
         drawString(g2, nodeData.getValue() + "", 12, 18, false, Color.white, new Font(Font.SERIF, Font.BOLD, 14));
@@ -32,6 +38,11 @@ public class UINode extends UIBufferedObject {
 
     @Override
     public void update() {
+        if (currentStatus < 254) {
+            currentStatus++;
+            mainColor = new Color(255, 0, 0, currentStatus);
+        }
+
         if (x == nextX && y == nextY) return;
 
         updateCoordsBounds(new Rectangle((int) nextX, (int) nextY, width, height));
